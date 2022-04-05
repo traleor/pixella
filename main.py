a343f79380c8049198566f04076fa3fc38df852d
itzomen <peng.akebuon2468@gmail.com>
2022-04-05 08:12:37 +0100

    setup production ready file structure

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import os
-
 from skimage import io
 from sklearn.cluster import KMeans
 import numpy as np
 
 
-def main():
-    path = os.getcwd()
-    # Load the image
-    # Read the image
-    image = io.imread("assets/black.png")
-    io.imshow(image)
-    # io.show()
-
+def KCompression(img, k):
+    """Compress an image using K-means clustering."""
     # Dimension of the original image
-    rows = image.shape[0]
-    cols = image.shape[1]
+    # io.imshow(img)
+    # io.show()
+    # if k argument is not provided, use the default value 8
+    if k is None:
+        print("k is not provided, using default value 8")
+        k = 8
+    rows = img.shape[0]
+    cols = img.shape[1]
 
     # Flatten the image
-    image = image.reshape(rows * cols, 3)
+    img = img.reshape(rows * cols, 3)
 
     # Implement k-means clustering to form k clusters
-    kmeans = KMeans(n_clusters=64)
-    kmeans.fit(image)
+    # size a liitle bit bigger with random_state=0
+    # kmeans = KMeans(n_clusters=8, random_state=0).fit(image)
+    kmeans = KMeans(n_clusters=k).fit(img)
 
     # Replace each pixel value with its nearby centroid
     compressed_image = kmeans.cluster_centers_[kmeans.labels_]
@@ -32,9 +32,34 @@ def main():
     compressed_image = compressed_image.reshape(rows, cols, 3)
 
     # Save and display output image
-    io.imsave("compressed_image_64.png", compressed_image)
-    io.imshow(compressed_image)
-    # io.show()
+    return compressed_image
+
+
+def main():
+    path = os.getcwd()
+    image_name = input(
+        "Enter the image name found in assets/images folder including its extension: "
+    )
+    print("Loading image...")
+    print("\n")
+
+    # Read the image
+    try:
+        image = io.imread(path + "/assets/images/" + image_name)
+    except Exception as e:
+        print("File not found", e)
+        return
+
+    # Compress the image
+    # select compression method 1. Kcompression 2. Kmeans
+    compression_method = int(input("Select compression method: 1. Kcompression 2. OtherCompression: "))
+    print("\n")
+    if compression_method == 1:
+        # 1. Compress the image with KCompression
+        compressed_image = KCompression(image, None)
+
+    # Save and display output image
+    io.imsave("out.png", compressed_image)
 
 
 if __name__ == "__main__":

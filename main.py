37d41eb7508a6bdfec41497c8da0d5604dac1ee4
itzomen <peng.akebuon2468@gmail.com>
2022-04-05 17:16:25 +0100

    added compression by resize

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os
 from skimage import io
 from sklearn.cluster import KMeans
 import numpy as np
+from skimage.transform import resize
 
 
-def KCompression(img, k):
+def KMeanCompression(img, k):
     """Compress an image using K-means clustering."""
     # Dimension of the original image
     # io.imshow(img)
@@ -35,13 +36,23 @@ def KCompression(img, k):
     return compressed_image
 
 
+def ResizeCompression(img, resize_factor):
+    # resize image
+    compressed_image = resize(
+        img,
+        (img.shape[0] // resize_factor, img.shape[1] // resize_factor),
+        anti_aliasing=True,
+    )
+    # compressed_image = rescale(img, 0.25, anti_aliasing=True)
+    return compressed_image
+
+
 def main():
     path = os.getcwd()
     image_name = input(
         "Enter the image name found in assets/images folder including its extension: "
     )
     print("Loading image...")
-    print("\n")
 
     # Read the image
     try:
@@ -52,14 +63,31 @@ def main():
 
     # Compress the image
     # select compression method 1. Kcompression 2. Kmeans
-    compression_method = int(input("Select compression method: 1. Kcompression 2. OtherCompression: "))
+    compression_method = int(
+        input("Select compression method: 1. KMeanCompression 2. ResizeCompression: ")
+    )
     print("\n")
     if compression_method == 1:
         # 1. Compress the image with KCompression
-        compressed_image = KCompression(image, None)
+        compressed_image = KMeanCompression(image, None)
+    elif compression_method == 2:
+        # 2. Compress the image with OtherCompression
+        resize_factor = int(input("Enter resize factor: "))
+        compressed_image = ResizeCompression(image, resize_factor)
+    else:
+        print("Invalid input")
+        return
 
     # Save and display output image
     io.imsave("out.png", compressed_image)
+
+    image_file_size = round(
+        os.path.getsize(path + "/assets/images/" + image_name) / 1024, 2
+    )
+    compressed_image_file_size = round(os.path.getsize("out.png") / 1024, 2)
+    print("Original file size: ", image_file_size, "KB")
+    print("Compressed file size: ", compressed_image_file_size, "KB")
+    print("Compression Difference: ", round(image_file_size - compressed_image_file_size, 2))
 
 
 if __name__ == "__main__":
